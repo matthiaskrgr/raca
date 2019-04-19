a7552e1c035e98c28a6b05e47532324de4dd5554
Matthias Krüger <matthias.krueger@famsik.de>
2019-04-19 13:48:25 +0200

    add rough json parsing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use std::fs;
-use std::fs::{create_dir, read_dir};
-use std::io::Write;
-use std::process::Command;
-
 fn main() {
     let clippy = std::process::Command::new("cargo")
         .arg("clippy")
         //    let clippy = std::process::Command::new(
         //        "/home/matthias/vcs/github/rust-clippy/target/debug/cargo-clippy",
         //    )
-        .arg("--all-targets")
-        .arg("--all-features")
+        // .arg("--all-targets")
+        //.arg("--all-features")
         .arg("--message-format=json")
         .args(&[
             "--",
@@ -28,7 +23,30 @@ fn main() {
     //println!("crate_dir: {}, cargo_target_dir {}", crate_dir, target_dir.display());
     //println!("output: {:?}", CLIPPY);
     let stderr = String::from_utf8_lossy(&clippy.stderr).to_string();
-    let stdout = String::from_utf8_lossy(&clippy.stdout).to_string();
+    let stdout = String::from_utf8_lossy(&clippy.stdout).to_string(); // json
+
+    stdout
+        .lines()
+        .map(|raw_line| serde_json::from_str(raw_line).unwrap())
+        .map(|x: serde_json::Value| x)
+        .filter(|x| x["reason"] == "compiler-message")
+        .for_each(|json| {
+            let pid = &json["package_id"].to_string();
+
+            let pkg = pid.split_whitespace().nth(0).unwrap();
+            let version = pid.split_whitespace().nth(1).unwrap();
+
+            let src = &json["message"]["spans"][0]["file_name"];
+
+            let id = &json["message"]["code"]["code"];
+
+            let msg = format!("{} {} {} {}", pkg, version, id, src);
+            println!("{}", msg);
+        });
+
+    //println!("err: {}", stderr);
+    //println!("out: {}", stdout);
+
     if stderr.starts_with("error: internal compiler error:")
         || stderr.starts_with("query stack during panic:")
         || stdout.starts_with("error: internal compiler error:")
